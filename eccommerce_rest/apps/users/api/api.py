79f79363bc0537cbdff99e21ffd7f9354ab74554
Axel Vazquez <axelvazmar0@gmail.com>
2025-07-23 12:46:10 -0600

    Decorador @api_view

--- eccommerce_rest/apps/users/api/api.py
+++ eccommerce_rest/apps/users/api/api.py
@@ -1,27 +1,24 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
-from apps.users.api.serializers import UserSerializer, TestUserSerializer
+from apps.users.api.serializers import UserSerializer #,TestUserSerializer
 
-class UserAPIView(APIView):
 
-    def get(self, request): # Creando la petición HTTP 'GET'
+    # def get(self, request): # Creando la petición HTTP 'GET'
         
-        users = User.objects.all() # Haciendo consulta SQL 'FROM users SELECT *'
-        users_serializers = UserSerializer(users, many=True) # Diciendole a DJANGO qué, debe listar. El 'many=True' es para decirle a DJANGO que debe listar todos los resultados de la consulta SQL
-
-        test_data = {
-            'name':'axel',
-            'email':'vazmar@gmail.com'
-        }
+    #     users = User.objects.all() # Haciendo consulta SQL 'FROM users SELECT *'
+    #     users_serializers = UserSerializer(users, many=True) # Diciendole a DJANGO qué, debe listar. El 'many=True' es para decirle a DJANGO que debe listar todos los resultados de la consulta SQL
         
-        test_user = TestUserSerializer(data = test_data,context = test_data)
+    #     return Response(users_serializers.data, status=status.HTTP_200_OK) # '.data' es para retornar la información serializada y convertida a JSON
 
-        if test_user.is_valid():
-            test_user.save() # Saving the user in the DB
-            print("Pasó validaciones")
-        else:
-            print(test_user.errors) # ".errors" mete los errores en las validaciones en un diccionario
-        
-        return Response(users_serializers.data, status=status.HTTP_200_OK) # '.data' es para retornar la información serializada y convertida a JSON
+@api_view(['GET', 'POST']) # Decorador @api_view para crear las peticiones GET y POST
+def user_api_view(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        users_serializers = UserSerializer(users, many=True)
+        return Response(users_serializers.data)
+    
+    elif request.method == 'POST':
+        print(request.data)
