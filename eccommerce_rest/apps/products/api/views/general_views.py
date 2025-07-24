8b9593e51b52c651abc4ed81d98cd282c2469d20
Axel Vazquez <axelvazmar0@gmail.com>
2025-07-24 13:37:24 -0600

    Generalizado para evitar la repetición de líneas de código

--- eccommerce_rest/apps/products/api/views/general_views.py
+++ eccommerce_rest/apps/products/api/views/general_views.py
@@ -1,34 +1,14 @@
 # Primero importaciones nativas
-from rest_framework import generics
 
 # Segundo las importaciones propias
-from apps.products.models import MeasureUnit, CategoryProduct, Indicator
+from apps.base.api import GeneralListAPIView
 from apps.products.api.serializers.general_serializers import MeasureUnitSerializer, CategoryProductSerializer, IndicatorSerializer
 
-class MeasureUnitListAPIView(generics.ListAPIView):
+class MeasureUnitListAPIView(GeneralListAPIView):
     serializer_class = MeasureUnitSerializer
 
-    def get_queryset(self):
-        """
-        Consulta/Queryset para convertir en JSON todos los objetos del modelo MeasureUnit
-        """
-        # Este queryset filtra por los objetos que su estado es True, osea que están activos
-        return MeasureUnit.objects.filter(state=True)
-
-class CategoryProductListAPIView(generics.ListAPIView):
+class CategoryProductListAPIView(GeneralListAPIView):
     serializer_class = CategoryProductSerializer
-
-    def get_queryset(self):
-        """
-        Queryset para convertir a JSON todos los objetos del modelo CategoryProduct
-        """
-        return CategoryProduct.objects.filter(state=True)
     
-class IndicatorListAPIView(generics.ListAPIView):
+class IndicatorListAPIView(GeneralListAPIView):
     serializer_class = IndicatorSerializer
-
-    def get_queryset(self):
-        """
-        Queryset para convertir a JSON todos los objetos del modelo Indicator
-        """
-        return Indicator.objects.filter(state=True)
