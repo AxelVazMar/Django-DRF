5142f4715817e377f46aa5c0dc0d2c0c3e7106e4
Axel Vazquez <axelvazmar0@gmail.com>
2025-08-25 17:56:54 -0600

    Añadiendo sistema de logout

--- ecommerce_rest/ecommerce_rest/urls.py
+++ ecommerce_rest/ecommerce_rest/urls.py
@@ -5,7 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from apps.users.views import Login
+from apps.users.views import Login, Logout
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,4 +29,5 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('logout/', Logout.as_view(), name = 'Logout')
 ]
