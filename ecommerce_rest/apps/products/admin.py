7da9785561934a931be9930a7f87532c29dcda03
Axel Vazquez <axelvazmar0@gmail.com>
2025-08-30 14:09:26 -0600

    minimo cambio en comentarios

--- ecommerce_rest/apps/products/admin.py
+++ ecommerce_rest/apps/products/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from apps.products.models import *
+from apps.products.models import MeasureUnit, CategoryProduct, Indicator, Product
 
 # Register your models here.
-class MeasureUnitAdmin(admin.ModelAdmin):
-    list_display = ('id', 'description') # <== Aquí le estamos diciendo como mostrar la info en el admin de Django
+
+class MeasureUnitAdmin(admin.ModelAdmin):                               
+    list_display = ('id', 'description') # <== Aquí le estamos diciendo como mostrar la info en el admin de Django 
 
 class CategoryProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'description')
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','name' ,'description')
+    list_display = ('id', 'name', 'description')
 
 admin.site.register(MeasureUnit, MeasureUnitAdmin)
 admin.site.register(CategoryProduct, CategoryProductAdmin)
